05199bc919e045cc30435189850c32594950381a
MaelSa <mael.salazard@g-mail.fr>
2019-10-18 11:11:14 +0200

    client modif

--- cli.py
+++ cli.py
@@ -9,34 +9,38 @@ connexion_avec_serveur.send(ts.encode())
 
 end = False
 name = input("Quel est votre nom ?:\n")
-(board, hand) = connexion_avec_serveur.recv(255).decode()
+connexion_avec_serveur.send(name.encode())
+board = connexion_avec_serveur.recv(255).decode()
+
 print(f'Le marché est : {board}')
 print(f'Votre main est : {hand}')
 while not end:
     tour = connexion_avec_serveur.recv(255).decode()
-    (board, ressources) = connexion_avec_serveur.recv(255).decode()
+    board = connexion_avec_serveur.recv(255).decode()
+    ressource = connexion_avec_serveur.recv(255).decode()
     print(board)
     print(ressources)
-    if tour:
+    if tour == 'votre':
         hand = connexion_avec_serveur.recv(255).decode()
         print(hand)
-        possible_choice = False
-        while not possible_choice
+        possible_choice = ''
+        while possible_choice != "true":
             choice = input('Choisissez parmi vendre, échanger, chameaux ou prendre\n')
             connexion_avec_serveur.send(choice.encode())
             possible_choice = connexion_avec_serveur.recv(255).decode()
 
         if choice == 'vendre':
+
         elif choice == 'chameaux':
             nb = connexion_avec_serveur.recv(255).decode()
             print(f'Vous avez récupéré {nb} chameaux')
         elif choice == 'échanger':
         elif choice == 'prendre':
-            ok_take = False
-            while not ok_take:
+            ok_take = ""
+            while ok_take != 'true':
                     choice = input("Quelle carte prendre ?\n")
                     connexion_avec_serveur.send(choice.encode())
-
+                    ok_take = connexion_avec_serveur.recv(255).decode()
 
 
 
